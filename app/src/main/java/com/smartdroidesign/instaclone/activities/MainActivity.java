95febad3766ce6a71dafbe3595da37eba9922829
Memnoc <mgenesiostara@zendesk.com>
2018-02-01 11:35:37 +0000

    - deleted fragment from the MainActivity   - created 4 separated fragment    - did some clean up on those fragments

--- app/src/main/java/com/smartdroidesign/instaclone/activities/MainActivity.java
+++ app/src/main/java/com/smartdroidesign/instaclone/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.smartdroidesign.instaclone;
+package com.smartdroidesign.instaclone.activities;
 
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -18,6 +18,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.TextView;
+
+import com.smartdroidesign.instaclone.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -90,40 +92,7 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
 
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
@@ -145,7 +114,22 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return 4;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch(position) {
+                case 0:
+                    return "HOME";
+                case 1:
+                    return "SEARCH";
+                case 2:
+                    return "ACTIVITY";
+                case 3:
+                    return  "PROFILE";
+            }
+            return null;
         }
     }
 }
