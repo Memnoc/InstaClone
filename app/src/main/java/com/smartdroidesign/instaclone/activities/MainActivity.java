bd3523962c8a3eb1a20368d34279f0973d672b27
Memnoc <mgenesiostara@zendesk.com>
2018-02-01 15:24:10 +0000

    - implemented tabs with names   - changed background of each fragment
       - imported drawables resources

--- app/src/main/java/com/smartdroidesign/instaclone/activities/MainActivity.java
+++ app/src/main/java/com/smartdroidesign/instaclone/activities/MainActivity.java
@@ -1,25 +1,24 @@
 package com.smartdroidesign.instaclone.activities;
 
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
-
 import com.smartdroidesign.instaclone.R;
+import com.smartdroidesign.instaclone.fragments.FeedActivityFragment;
+import com.smartdroidesign.instaclone.fragments.HomeFragment;
+import com.smartdroidesign.instaclone.fragments.ProfileFragment;
+import com.smartdroidesign.instaclone.fragments.SearchFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -59,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setImageDrawable(ContextCompat.getDrawable(getBaseContext(), R.drawable.activity_btn));
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -106,30 +106,22 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+
+            switch (position){
+                case 0:
+                    return HomeFragment.newInstance();
+                case 1:
+                    return SearchFragment.newInstance();
+                case 2:
+                    return FeedActivityFragment.newInstance();
+                 default:
+                     return ProfileFragment.newInstance();
+            }
         }
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
             return 4;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch(position) {
-                case 0:
-                    return "HOME";
-                case 1:
-                    return "SEARCH";
-                case 2:
-                    return "ACTIVITY";
-                case 3:
-                    return  "PROFILE";
-            }
-            return null;
         }
     }
 }
