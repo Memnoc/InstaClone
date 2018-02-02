1b7bb8b5467703a8a0b6b4baa920a836db9d3813
Memnoc <mgenesiostara@zendesk.com>
2018-02-02 10:37:55 +0000

    - implemented tabs with names   - changed background of each fragment
       - added two new activities
         - MediaActivity (placeholder for the camera call)
         - SettingsActivity (placeholder for Settings screen)
          - Added one more option to the dots menu "Privacy Policy"

--- app/src/main/java/com/smartdroidesign/instaclone/activities/MainActivity.java
+++ app/src/main/java/com/smartdroidesign/instaclone/activities/MainActivity.java
@@ -1,8 +1,9 @@
 package com.smartdroidesign.instaclone.activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -14,6 +15,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
 import com.smartdroidesign.instaclone.R;
 import com.smartdroidesign.instaclone.fragments.FeedActivityFragment;
 import com.smartdroidesign.instaclone.fragments.HomeFragment;
@@ -59,11 +61,17 @@ public class MainActivity extends AppCompatActivity {
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setImageDrawable(ContextCompat.getDrawable(getBaseContext(), R.drawable.camera_icon));
+
+
+        final Context context = this;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
+                Intent intent = new Intent(context, MediaActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -86,6 +94,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
